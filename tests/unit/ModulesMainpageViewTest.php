227809aca7daced8ec17c462c37db4a3b6cd295a
enikeishik <enikeishik@gmail.com>
2018-09-24 14:54:59 +0300

    fix: remove travis-ci vm id from var_dump

--- tests/unit/ModulesMainpageViewTest.php
+++ tests/unit/ModulesMainpageViewTest.php
@@ -48,11 +48,10 @@ class ModulesMainpageViewTest extends ModulesAbstractViewTest
     {
         //remove travis-ci VM id from var_dump
         $content = preg_replace('/ \/tmp\/tmp.+\n/', ' ', $this->getRenderContent());
-        var_dump($content); exit();
         $this->assertTrue(false !== strpos($content, 'test view template begin'));
         $this->assertTrue(false !== strpos($content, '$item array(2)'));
-        $this->assertTrue(false !== strpos($content, '["id"]=>'));
-        $this->assertTrue(false !== strpos($content, '["body"]=>'));
+        $this->assertTrue(false !== strpos($content, 'id'));
+        $this->assertTrue(false !== strpos($content, 'body'));
         $this->assertTrue(false !== strpos($content, '$items NULL'));
         $this->assertTrue(false !== strpos($content, 'test view template end'));
     }
